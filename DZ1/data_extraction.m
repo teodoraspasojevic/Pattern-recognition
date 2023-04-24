4b572b167164559ea9d71df7be9ae9430174d59e
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-04-24 21:50:12 +0200

    Delete DZ1 directory

--- DZ1/data_extraction.m
+++
@@ -1,9 +0,0 @@
-function [speed_x, speed_y, pressure,position_x,position_y] = data_extraction(A1)
-    
-speed_x = A1(1,:);
-    speed_y = A1(2,:);
-    pressure = A1(3,:);
-    position_x = cumsum(speed_x);
-    position_y = cumsum(speed_y);
-    
-end
